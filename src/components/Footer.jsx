e38f95e1d9210690de9af839ee150e0617ce15ed
Rahul32-code <114091763+Rahul32-code@users.noreply.github.com>
2025-09-13 14:39:53 +0530

    footer date update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="container mx-auto px-6 py-6 mt-10">
       <div className="bg-white/10 border border-white/20 backdrop-blur-md rounded-xl shadow-[0_0_20px_rgba(255,255,255,0.1)] text-white text-center py-6">
         <p className="text-sm md:text-base font-medium">
-          &copy; {new Date().getFullYear()}{" "}
+          &copy; 2025{" "}
           <span className="text-teal-400 font-semibold text-glow-pulse-teal">
             Rahul
           </span>
